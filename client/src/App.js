a777ded3545cc3c0dd7ea2b853ea950ad0ceee04
Yash Hegde <yash4success@gmail.com>
2023-09-07 17:47:36 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/auth/Login'; // Import the Login component
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'; // Import Navigate
+import Login from './components/auth/Login';
 import FacebookLogin from '@greatsumini/react-facebook-login';
 import axios from 'axios';
 import AgentScreen from './components/AgentScreen';
@@ -8,7 +8,7 @@ import AgentScreen from './components/AgentScreen';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false); // State to track if the user is logged in
+  const [loggedIn, setLoggedIn] = useState(false);
 
   const responseFacebook = (response) => {
     axios({
@@ -30,19 +30,22 @@ function App() {
   }
 
   return (
-    <div className="col-md-6 offset-md-3 text-center">
-      <h1>Login with Facebook</h1>
-      <FacebookLogin
-        appId="327089429768358"
-        autoLoad={false}
-        callback={responseFacebook}
-      />
-      {loggedIn ? (
-        <div className="App">
-          <AgentScreen />
-        </div>
-      ) : null}
-    </div>
+    <Router>
+      <div className="col-md-6 offset-md-3 text-center">
+        <h1>Login with Facebook</h1>
+        <FacebookLogin
+          appId="327089429768358"
+          autoLoad={false}
+          callback={responseFacebook}
+        />
+        {loggedIn ? (
+          <Navigate to="/agentscreen" /> // Redirect to AgentScreen
+        ) : null}
+      </div>
+      <Routes>
+        <Route path="/agentscreen" element={<AgentScreen />} />
+      </Routes>
+    </Router>
   );
 }
 
